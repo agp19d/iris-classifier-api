23f016121e216c3f7e127183daf49f8c7ee74ba1
agp19d <agp19d@gmail.com>
2023-08-19 12:35:24 -0500

    added comments to predict_iris.py

--- python_scripts/predict_iris.py
+++ python_scripts/predict_iris.py
@@ -1,26 +1,40 @@
 # predict_iris.py
+
+# Import necessary libraries and modules
 import sys
 from joblib import load
 import numpy as np
 import os
 
 def main():
+    # Ensure that the user provides exactly 4 features
     if len(sys.argv) != 5:
         print("You must provide exactly 4 features for prediction.")
         return
 
+    # Convert command-line arguments to floating-point numbers
     input_features = list(map(float, sys.argv[1:]))
 
+    # Check for invalid input features (NaN or infinity)
     if any(np.isnan(input_features)) or any(np.isinf(input_features)):
         print("Invalid input features detected.")
         return
     
+    # Debug: print the current working directory
     print(os.getcwd())
+
+    # Load the trained machine learning model
     model = load('./python_scripts/iris_model.pkl')
+
+    # Make a prediction using the provided features
     prediction = model.predict([input_features])
 
+    # Define the names of the iris classes
     iris_classes = ["setosa", "versicolor", "virginica"]
+
+    # Output the predicted class
     print(f"Predicted class index: {prediction[0]} ({iris_classes[prediction[0]]})")
 
+# Ensure the script is being run as the main module
 if __name__ == "__main__":
     main()
